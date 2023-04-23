e7718e1bf85fabb44f88d7f23cbde7d00de0c5c2
Francisco Aires <xicojr@gmail.com>
2023-04-23 12:35:44 -0300

    teste CrossOrigin

--- src/main/java/com/example/cafedamanha/controller/EmployeeController.java
+++ src/main/java/com/example/cafedamanha/controller/EmployeeController.java
@@ -25,8 +25,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "https://desafio-unidac-angular.netlify.app/")
-// @CrossOrigin(origins = "http://localhost:4200")
+// @CrossOrigin(origins = "https://desafio-unidac-angular.netlify.app/")
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/employees")
 public class EmployeeController {
 
